b437988ff6c4972f320942194fa6a8f5dae57170
Anastasia Remizova <anastasia.remizova@epfl.ch>
2025-12-09 22:32:33 +0100

    speeding up LD training

--- sim/ld/setup.py
+++ sim/ld/setup.py
@@ -1,10 +1,27 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
+extensions = [
+    Extension(
+        "rng",
+        ["rng.pyx"],
+        extra_compile_args=["-O3"],
+        include_dirs=[np.get_include()],
+    ),
+    Extension(
+        "trainLD",
+        ["trainLD.pyx"],
+        extra_compile_args=["-O3"],
+        include_dirs=[np.get_include()],
+    ),
+]
+
 setup(
-    name='trainLD',
-    ext_modules=cythonize('trainLD.pyx'),
+    name="ld_project",
+    ext_modules=cythonize(
+        extensions,
+        language_level="3",
+    ),
     zip_safe=False,
-    include_dirs=[np.get_include()]
 )
--- sim/ld/trainLD.pyx
+++ sim/ld/trainLD.pyx
@@ -6,11 +6,26 @@ cimport cython
 import numpy as np
 cimport numpy as np
 from libc.stdlib cimport malloc, free
+from libc.stdint cimport uint64_t
+from rng cimport XorShift64Star
+
+
+cpdef uint64_t make_numpy_seed():
+    """
+    Generate a uniform 64-bit seed using old NumPy RNG (32-bit based).
+    """
+    high = np.random.randint(0, 1 << 32, dtype=np.uint32)
+    low  = np.random.randint(0, 1 << 32, dtype=np.uint32)
+
+    return (<uint64_t> int(high) << 32) | <uint64_t> int(low)
+
 
 cpdef trainLD(int p, int nS, double lr, double[:,:] X, double[:] y, double[:] beta0, np.int64_t[:] plot_list,
 int n, int d, int n_it_max, int n_runs, double epsilon):
-    cdef int k, r, s, q, N
+    cdef XorShift64Star rng
+    cdef int k, r, s, N
     cdef double gd_aux0, gd_aux1, diff_coeff
+    cdef double inv_n_runs = 1.0 / n_runs
     #####
     cdef double *arr = <double*>malloc(p * sizeof(double))
     if arr == NULL:
@@ -37,7 +52,18 @@ int n, int d, int n_it_max, int n_runs, double epsilon):
         raise MemoryError()
     cdef double[:,:] XS = <double[:n, :p]>arr6
     #####
+    cdef double *arr_noise = <double*>malloc(p * sizeof(double))
+    if arr_noise == NULL:
+        raise MemoryError()
+    cdef double[:] noise = <double[:p]>arr_noise
+    #####
     cdef np.int64_t[::1] S_view
+    #####
+    cdef np.ndarray[np.float64_t, ndim=2] betaS_av
+    cdef double[:, ::1] betaS_mv
+    cdef Py_ssize_t n_plots
+    cdef Py_ssize_t count_aux = 1
+    cdef long next_plot_k
 
     try:
         diff_coeff = np.sqrt(2 * epsilon)
@@ -47,34 +73,42 @@ int n, int d, int n_it_max, int n_runs, double epsilon):
 
         ###### Initializing the estimator
         betaS0[:] = 0.
-        for q in range(p):
-            betaS0[q] = beta0[S_view[q]]
+        for s in range(p):
+            betaS0[s] = beta0[S_view[s]]
         del beta0
 
         ###### Constructing XS
         for r in range(n):
-            for q in range(p):
-                XS[r, q] = X[r, S_view[q]]
+            for s in range(p):
+                XS[r, s] = X[r, S_view[s]]
         del X
         
-        ######## LANGEVIN DIFFUSION
+        ######## Preparing array for beta evolution
+        n_plots = plot_list.shape[0]
+        betaS_av = np.zeros((n_plots + 1, p))
+        betaS_mv = betaS_av
+        for s in range(p):
+            betaS_mv[0, s] = betaS0[s]
+        if n_plots > 0:
+            next_plot_k = plot_list[0]
+        else:
+            next_plot_k = -1
 
-        betaS_av = np.zeros((len(plot_list) + 1, p))
-        betaS_av[0] = np.array(betaS0)
 
-        noise = np.empty(p, dtype=np.float64)
-
+        rng = XorShift64Star(make_numpy_seed())
         for N in range(n_runs):
 
             ### N-th stochastic run
             betaS[:] = betaS0[:]
             #######
-            count_aux = 1
-            for k in range(1, n_it_max+1):
+            for k in range(1, n_it_max + 1):
                 #### GD ####
-                ## Inner product: XS and beta_k 
-                D[:] = 0. 
-                GRAD[:] = 0.
+                ## Inner product: XS and beta_k
+                for r in range(n):
+                    D[r] = 0. 
+                for s in range(p):
+                    GRAD[p] = 0.
+                
                 for r in range(n):
                     gd_aux0 = 0.
                     for s in range(p):
@@ -86,17 +120,20 @@ int n, int d, int n_it_max, int n_runs, double epsilon):
                     for r in range(n):
                         gd_aux1 += XS[r, s] * D[r]
                     GRAD[s] = gd_aux1
+                rng.fill_gaussian_c(noise)
                 ## GD update
-                noise[:] = np.random.randn(p)
                 for s in range(p):
                     betaS[s] += lr * (GRAD[s] / n + diff_coeff * noise[s])
 
-                ## Retrieving dynamics
-                save_k = k in plot_list
-                if save_k:
-                    betaS_av[count_aux] += np.array(betaS) / n_runs
+                ## Retrieving dynamics 
+                if k == next_plot_k:
+                    for s in range(p):
+                        betaS_mv[count_aux, s] += betaS[s] * inv_n_runs
                     count_aux += 1
-            # print(np.asarray(betaS))
+                    if count_aux <= n_plots:
+                        next_plot_k = plot_list[count_aux - 1]
+                    else:
+                        next_plot_k = -1
 
         return betaS_av
 
@@ -106,4 +143,5 @@ int n, int d, int n_it_max, int n_runs, double epsilon):
         free(arr6)
         free(arr5)
         free(arr)
+        free(arr_noise)
 
