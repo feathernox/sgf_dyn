29befdb7417a17e7827045ee4f578d929e84c1ec
Anastasia Remizova <anastasia.remizova@epfl.ch>
2026-05-18 17:23:25 +0200

    reorganized file, legacied one of the functions, added beta2 argument to etrain/etest functions

--- theory/theory.py
+++ theory/theory.py
@@ -39,116 +39,6 @@ def MP_expectation(f, alpha):
     return exp_val
 
 
-
-def Etrain_inf_time(alpha, psi, l2): 
-    # no dependency on betadiff2
-    res = (1 - alpha / psi + l2) * (1 - np.minimum(alpha, 1))
-    return res
-    
-    
-def Etrain_inf_time_precise(p, n, d, l2): 
-    # no dependency on betadiff2
-    res = (1 - p / d + l2) * (1 - np.minimum(p, n) / n)
-    return res
-    
-
-def _Etest_inf_time_alpha_leq_1(alpha, psi, l2, betadiff2):
-    res = (1 - alpha / psi + l2) * 1 / (1 - alpha)
-    return res
-
-
-def _Etest_inf_time_alpha_g_1(alpha, psi, l2, betadiff2):
-    res = alpha / psi * betadiff2 * (1 - 1 / alpha) + (1 - alpha / psi + l2) * (1 + 1 / (alpha - 1))
-    return res
-    
-    
-def Etest_inf_time(alpha, psi, l2, betadiff2):
-    alpha = np.asarray(alpha)
-    res = np.zeros(alpha.shape)
-    
-    is_alpha_leq_1 = alpha <= 1 
-    res[is_alpha_leq_1] = _Etest_inf_time_alpha_leq_1(alpha[is_alpha_leq_1], psi, l2, betadiff2)
-    res[~is_alpha_leq_1] = _Etest_inf_time_alpha_g_1(alpha[~is_alpha_leq_1], psi, l2, betadiff2)
-    return res
-
-
-def Etest_inf_time_precise(p, n, d, l2, betadiff2):
-    p = np.asarray(p)
-    res = np.full(p.shape, np.inf)
-    
-    is_p_l_nm1 = p < (n - 1)
-    p_l_nm1 = p[is_p_l_nm1]
-    res[is_p_l_nm1] = (1 - p_l_nm1 / d + l2) * (1 + p_l_nm1 / (n - p_l_nm1 - 1))
-    
-    is_p_g_np1 = p > (n + 1)
-    p_g_np1 = p[is_p_g_np1]
-    res[is_p_g_np1] = p_g_np1 / d * betadiff2 * (1 - n / p_g_np1) + (1 - p_g_np1 / d + l2) * (1 + n / (p_g_np1 - n - 1))
-    
-    return res
-
-
-def _Etrain_alpha_leq_1(alpha, psi, l2, t, betadiff2):
-    f1 = lambda x: x * math.exp(-2 * t * x)
-    f2 = lambda x: math.exp(-2 * t * x)
-    res = alpha * betadiff2 / psi * MP_expectation(f1, alpha) + \
-          (1 - alpha / psi + l2) * (1 - alpha + alpha * MP_expectation(f2, alpha))
-    return res
-
-
-def _Etrain_alpha_g_1(alpha, psi, l2, t, betadiff2):
-    f1 = lambda x: x * math.exp(-2 * t * alpha * x)
-    f2 = lambda x: math.exp(-2 * t * alpha * x)
-    res =  betadiff2 / psi * alpha * MP_expectation(f1, 1 / alpha) + \
-            (1 - alpha / psi + l2) * MP_expectation(f2, 1 / alpha)
-    return res
-
-
-_Etrain_alpha_leq_1 = np.vectorize(_Etrain_alpha_leq_1)
-_Etrain_alpha_g_1 = np.vectorize(_Etrain_alpha_g_1)
-
-
-def Etrain(alpha, psi, l2, t, betadiff2):
-    if alpha <= 1:
-        return _Etrain_alpha_leq_1(alpha, psi, l2, t, betadiff2)
-    else:
-        return _Etrain_alpha_g_1(alpha, psi, l2, t, betadiff2)
-    
-Etrain = np.vectorize(Etrain)
-
-
-def _Etest_alpha_leq_1(alpha, psi, l2, t, betadiff2):
-    f1 = lambda x: math.exp(-2 * t * x)
-    f2 = lambda x: (1 - math.exp(- t * x)) ** 2 / x
-    res = alpha * betadiff2 / psi * MP_expectation(f1, alpha) + \
-          (1 - alpha / psi + l2) * (1 + alpha * MP_expectation(f2, alpha))
-    return res
-
-
-def _Etest_alpha_g_1(alpha, psi, l2, t, betadiff2):
-    f1 = lambda x: math.exp(-2 * alpha * t * x)
-    f2 = lambda x: (1 - math.exp(- alpha * t * x)) ** 2 / x
-    res = betadiff2 * (alpha - 1) / psi + betadiff2 / psi * MP_expectation(f1, 1 / alpha) + \
-          (1 - alpha / psi + l2) * (1 + 1 / alpha * MP_expectation(f2, 1 / alpha))
-    return res
-
-
-_Etest_alpha_leq_1 = np.vectorize(_Etest_alpha_leq_1)
-_Etest_alpha_g_1 = np.vectorize(_Etest_alpha_g_1)
- 
-
-def Etest(alpha, psi, l2, t, betadiff2):
-    """ 
-    No factor 1 / 2 applied here
-    """
-    if alpha <= 1:
-        return _Etest_alpha_leq_1(alpha, psi, l2, t, betadiff2)
-    else:
-        return _Etest_alpha_g_1(alpha, psi, l2, t, betadiff2)
-
-Etest = np.vectorize(Etest)
-
-
-
 def expectation_MP_exp(alpha, t, eps=1e-2, max_i1_arg=100):
     res = 1 / np.sqrt(alpha) * odeint(
         lambda y, s:  np.where(
@@ -164,10 +54,123 @@ def expectation_MP_exp(alpha, t, eps=1e-2, max_i1_arg=100):
     return res[1:, 0]
 
 
+##### TRAIN ERROR OLD ######
+
+def Etrain_inf_time(alpha, psi, l2, beta2=1.): 
+    # no dependency on betadiff2
+    res = ((1 - alpha / psi) * beta2 + l2) * (1 - np.minimum(alpha, 1))
+    return res
+    
+    
+def Etrain_inf_time_precise(p, n, d, l2, beta2=1.): 
+    # no dependency on betadiff2
+    res = ((1 - p / d) * beta2 + l2) * (1 - np.minimum(p, n) / n)
+    return res
+
+
+def _Etrain_alpha_leq_1(alpha, psi, l2, t, betadiff2, beta2=1.):
+    f1 = lambda x: x * math.exp(-2 * t * x)
+    f2 = lambda x: math.exp(-2 * t * x)
+    res = alpha * betadiff2 / psi * MP_expectation(f1, alpha) + \
+          ((1 - alpha / psi) * beta2 + l2) * (1 - alpha + alpha * MP_expectation(f2, alpha))
+    return res
+
+
+def _Etrain_alpha_g_1(alpha, psi, l2, t, betadiff2, beta2=1.):
+    f1 = lambda x: x * math.exp(-2 * t * alpha * x)
+    f2 = lambda x: math.exp(-2 * t * alpha * x)
+    res =  betadiff2 / psi * alpha * MP_expectation(f1, 1 / alpha) + \
+            ((1 - alpha / psi) * beta2 + l2) * MP_expectation(f2, 1 / alpha)
+    return res
+
+
+_Etrain_alpha_leq_1 = np.vectorize(_Etrain_alpha_leq_1)
+_Etrain_alpha_g_1 = np.vectorize(_Etrain_alpha_g_1)
+
+
+def Etrain(alpha, psi, l2, t, betadiff2, beta2=1.):
+    if alpha <= 1:
+        return _Etrain_alpha_leq_1(alpha, psi, l2, t, betadiff2, beta2=beta2)
+    else:
+        return _Etrain_alpha_g_1(alpha, psi, l2, t, betadiff2, beta2=beta2)
+    
+Etrain = np.vectorize(Etrain)
+
+
+##### TEST ERROR OLD ######
+
+
+def _Etest_inf_time_alpha_leq_1(alpha, psi, l2, betadiff2, beta2=1.):
+    res = ((1 - alpha / psi) * beta2 + l2) * 1 / (1 - alpha)
+    return res
+
+
+def _Etest_inf_time_alpha_g_1(alpha, psi, l2, betadiff2, beta2=1.):
+    res = alpha / psi * betadiff2 * (1 - 1 / alpha) + ((1 - alpha / psi) * beta2 + l2) * (1 + 1 / (alpha - 1))
+    return res
+    
+    
+def Etest_inf_time(alpha, psi, l2, betadiff2, beta2=1.):
+    alpha = np.asarray(alpha)
+    res = np.zeros(alpha.shape)
+    
+    is_alpha_leq_1 = alpha <= 1 
+    res[is_alpha_leq_1] = _Etest_inf_time_alpha_leq_1(alpha[is_alpha_leq_1], psi, l2, betadiff2, beta2=beta2)
+    res[~is_alpha_leq_1] = _Etest_inf_time_alpha_g_1(alpha[~is_alpha_leq_1], psi, l2, betadiff2, beta2=beta2)
+    return res
+
+
+def Etest_inf_time_precise(p, n, d, l2, betadiff2, beta2=1.):
+    p = np.asarray(p)
+    res = np.full(p.shape, np.inf)
+    
+    is_p_l_nm1 = p < (n - 1)
+    p_l_nm1 = p[is_p_l_nm1]
+    res[is_p_l_nm1] = ((1 - p_l_nm1 / d) * beta2 + l2) * (1 + p_l_nm1 / (n - p_l_nm1 - 1))
+    
+    is_p_g_np1 = p > (n + 1)
+    p_g_np1 = p[is_p_g_np1]
+    res[is_p_g_np1] = p_g_np1 / d * betadiff2 * (1 - n / p_g_np1) + ((1 - p_g_np1 / d) * beta2 + l2) * (1 + n / (p_g_np1 - n - 1))
+    
+    return res
+
+
+def _legacy_Etest_alpha_leq_1(alpha, psi, l2, t, betadiff2, beta2=1.):
+    f1 = lambda x: math.exp(-2 * t * x)
+    f2 = lambda x: (1 - math.exp(- t * x)) ** 2 / x
+    res = alpha * betadiff2 / psi * MP_expectation(f1, alpha) + \
+          ((1 - alpha / psi) * beta2 + l2) * (1 + alpha * MP_expectation(f2, alpha))
+    return res
+
+
+def _legacy_Etest_alpha_g_1(alpha, psi, l2, t, betadiff2, beta2=1.):
+    f1 = lambda x: math.exp(-2 * alpha * t * x)
+    f2 = lambda x: (1 - math.exp(- alpha * t * x)) ** 2 / x
+    res = betadiff2 * (alpha - 1) / psi + betadiff2 / psi * MP_expectation(f1, 1 / alpha) + \
+          ((1 - alpha / psi) * beta2 + l2) * (1 + 1 / alpha * MP_expectation(f2, 1 / alpha))
+    return res
+
+
+_legacy_Etest_alpha_leq_1 = np.vectorize(_legacy_Etest_alpha_leq_1)
+_legacy_Etest_alpha_g_1 = np.vectorize(_legacy_Etest_alpha_g_1)
+ 
+
+def legacy_Etest(alpha, psi, l2, t, betadiff2, beta2=1.):
+    """ 
+    No factor 1 / 2 applied here
+    """
+    if alpha <= 1:
+        return _legacy_Etest_alpha_leq_1(alpha, psi, l2, t, betadiff2, beta2=beta2)
+    else:
+        return _legacy_Etest_alpha_g_1(alpha, psi, l2, t, betadiff2, beta2=beta2)
+
+legacy_Etest = np.vectorize(legacy_Etest)
+
+
 
 def get_z_covariation(alpha, psi, l2, t, betadiff2, eps=1e-15):
     """
-    TODO: I think I need to remove 1/2 here for consistency
+    TODO: I think I need to remove 1/2 here for consistency or add it to Etest
     """
 #     alpha = np.asarray(alpha)
 
